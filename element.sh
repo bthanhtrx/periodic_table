93fb15abf9c561efb1094ebd2f9e3c2a86a02071
BThanh T <tranbaothanh2208@gmail.com>
2024-11-02 15:50:21 +0000

    feat: handle logic when the input argument not found in the db

--- element.sh
+++ element.sh
@@ -15,11 +15,17 @@ else
 
   else
     QUERY_RESULT=$($PSQL "select atomic_number, symbol, name, atomic_mass, melting_point_celsius, boiling_point_celsius, type from properties inner join elements using(atomic_number) inner join types using(type_id) WHERE name='$1'")
-    echo fullname
+
   fi
-  echo $QUERY_RESULT | while IFS=' | ' read ATOMIC_NUMBER SYMBOL NAME MASS MELTING_POINT BOILING_POINT TYPE
-  do
-    echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
-  done
+
+  if [[ -z $QUERY_RESULT ]]
+  then
+    echo -e "\nI could not find that element in the database."
+  else
+    echo $QUERY_RESULT | while IFS=' | ' read ATOMIC_NUMBER SYMBOL NAME MASS MELTING_POINT BOILING_POINT TYPE
+    do
+      echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+    done
+  fi
 
 fi
