9c41cdcc415ab7528a002fdbbd9e2484623b969f
BThanh T <tranbaothanh2208@gmail.com>
2024-11-02 15:52:52 +0000

    refactor: clean whitespaces

--- element.sh
+++ element.sh
@@ -7,15 +7,11 @@ else
   if [[ $1 =~ ^[0-9]+$ ]]
   then
     QUERY_RESULT=$($PSQL "select atomic_number, symbol, name, atomic_mass, melting_point_celsius, boiling_point_celsius, type from properties inner join elements using(atomic_number) inner join types using(type_id) WHERE atomic_number='$1'")
-
-
   elif [[ $1 =~ ^[A-Z][a-z]{0,1}$ ]]
   then
     QUERY_RESULT=$($PSQL "select atomic_number, symbol, name, atomic_mass, melting_point_celsius, boiling_point_celsius, type from properties inner join elements using(atomic_number) inner join types using(type_id) WHERE symbol='$1'")
-
   else
     QUERY_RESULT=$($PSQL "select atomic_number, symbol, name, atomic_mass, melting_point_celsius, boiling_point_celsius, type from properties inner join elements using(atomic_number) inner join types using(type_id) WHERE name='$1'")
-
   fi
 
   if [[ -z $QUERY_RESULT ]]
@@ -27,5 +23,4 @@ else
       echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
     done
   fi
-
 fi
